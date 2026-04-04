73fef4ba90e4ab092dcd520b3b36779f440765cc
earian1 | WaterDrinker49 <lemmu2185@gmail.com>
2026-04-03 19:23:47 -0700

    new commit to guest_branch, included guest portion code

--- SoundManager/Source/MainComponent.h
+++ SoundManager/Source/MainComponent.h
@@ -2,138 +2,137 @@
 
 #include <JuceHeader.h>
 
-// Forward declaration
-class BufferAudioSource;
-
 class MainComponent final
     : public juce::Component,
-      public juce::MenuBarModel,
-      public juce::AudioIODeviceCallback,
-      public juce::Timer
-  
+    public juce::MenuBarModel,
+    public juce::AudioIODeviceCallback,
+    public juce::ListBoxModel
 {
 public:
     explicit MainComponent(juce::ApplicationProperties& props);
     ~MainComponent() override;
-    
-        void saveRecording();
-        void playRecordedAudio();
-        
 
     void paint(juce::Graphics&) override;
     void resized() override;
-    
 
-    // Menu bar
     juce::StringArray getMenuBarNames() override;
     juce::PopupMenu getMenuForIndex(int, const juce::String&) override;
     void menuItemSelected(int, int) override;
 
-    // Audio callback
-void audioDeviceIOCallbackWithContext (const float* const* inputChannelData,
-                                       int numInputChannels,
-                                       float* const* outputChannelData,
-                                       int numOutputChannels,
-                                       int numSamples,
-                                       const juce::AudioIODeviceCallbackContext& context) override;
+    void audioDeviceIOCallbackWithContext(const float* const* inputChannelData,
+        int numInputChannels,
+        float* const* outputChannelData,
+        int numOutputChannels,
+        int numSamples,
+        const juce::AudioIODeviceCallbackContext& context) override;
 
     void audioDeviceAboutToStart(juce::AudioIODevice* device) override;
     void audioDeviceStopped() override;
 
+    int getNumRows() override;
+    void paintListBoxItem(int rowNumber,
+        juce::Graphics& g,
+        int width,
+        int height,
+        bool rowIsSelected) override;
+    void selectedRowsChanged(int lastRowSelected) override;
+
 private:
     enum class AppState
     {
-        FIRST_USER_SETUP,
         LOGIN,
         MAIN_APP
     };
 
-    // Playback
-    std::unique_ptr<BufferAudioSource> bufferSource;
-    juce::AudioBuffer<float> playbackBuffer;
+    juce::PropertiesFile* userStorage{ nullptr };
+    AppState currentState{ AppState::LOGIN };
+    bool loggedInAsGuest{ false };
 
-    juce::PropertiesFile* userStorage { nullptr };
-    AppState currentState { AppState::FIRST_USER_SETUP };
-
-    // FIRST USER SETUP
-    juce::Label usernameLabel_setup, passwordLabel_setup, accountInfoLabel_setup, roleLabel_setup;
-    juce::TextEditor usernameField_setup, passwordField_setup, accountInfoField_setup;
-    juce::ComboBox roleSelector_setup;
-    juce::TextButton submitButton { "Submit" };
-    juce::TextButton backButton { "Back" };
-
-    // LOGIN
-    juce::Label usernameLabel_login, passwordLabel_login;
+    // Login UI
+    juce::Label usernameLabel_login, passwordLabel_login, loginRoleLabel;
     juce::TextEditor usernameField_login, passwordField_login;
-    juce::Label roleDisplay_login;
-    juce::TextButton loginButton { "Login" };
+    juce::ComboBox loginRoleSelector;
+    juce::TextButton loginButton{ "Login" };
 
-    // MAIN APP
+    // Main app labels
     juce::Label mainAppPlaceholder;
+    juce::Label soundsListLabel;
+    juce::Label waveformLabel;
+    juce::Label clusterMapLabel;
+    juce::Label recordingStatusLabel;
 
     // Menu bar
     juce::MenuBarComponent menuBar;
 
-    // Owner dashboard
-    juce::TextButton recordButton { "Record" };
-    juce::TextButton playButton { "Play" };
-    juce::TextButton stopButton { "Stop" };
-    juce::TextButton saveButton { "Save" };
-    juce::TextButton createGuestButton { "Create Guest Account" };
+    // Buttons
+    juce::TextButton recordButton{ "Record" };
+    juce::TextButton playButton{ "Play" };
+    juce::TextButton stopButton{ "Stop" };
+    juce::TextButton saveButton{ "Save" };
+    juce::TextButton createGuestButton{ "Create Guest Account" };
+    juce::TextButton logoutButton{ "Logout" };
+    juce::TextButton downloadButton{ "Download Sound" };
+
+    // Sound list
+    juce::ListBox soundList;
+    juce::StringArray savedSoundNames;
+    juce::StringArray savedSoundPaths;
+    int selectedSoundRow = -1;
 
     // Audio
     juce::AudioFormatManager formatManager;
     juce::AudioTransportSource transportSource;
+    std::unique_ptr<juce::AudioFormatReaderSource> currentAudioFile;
     juce::AudioDeviceManager deviceManager;
 
     // Recording
     juce::AudioBuffer<float> recordingBuffer;
-    bool isRecording { false };
+    bool isRecording{ false };
     int recordingPosition = 0;
-    bool showRecordingDot { true };
-
-    // Recording status
-    juce::Label recordingStatusLabel;
-
-    // Sound list
-    juce::ListBox soundList;
+    double currentSampleRate = 44100.0;
 
     // Sliders
     juce::Slider pitchSlider, lengthSlider, volumeSlider;
     juce::Label pitchLabel, lengthLabel, volumeLabel;
 
-    // Cluster map placeholder
-    juce::Component clusterMapPlaceholder;
+    // Drawing areas
+    juce::Rectangle<int> waveformArea;
+    juce::Rectangle<int> clusterArea;
 
-    // File chooser
-    std::unique_ptr<juce::FileChooser> fileChooser;
-
-    // Timer for updating playback position
-    juce::TextButton pauseButton { "Pause" };
-    juce::TextButton resumeButton { "Resume" };
-    juce::Slider positionSlider;
-    juce::Label positionLabel;
-    bool isScrubbing { false };
-
-    int blinkCounter { 0 };
-
-
-    // Helpers
     void setupUI();
     void updateVisibility();
-    bool userExists() const;
-    void saveUserInfo(const juce::String&, const juce::String&, const juce::String&, const juce::String&);
-    void loadUserInfo(juce::String&, juce::String&, juce::String&, juce::String&) const;
-    void timerCallback() override;
 
-    juce::Component recordingDot;
+    void ensureDefaultAccounts();
+    bool roleChoiceUnlocked() const;
+    void setRoleChoiceUnlocked(bool unlocked);
 
-    juce::String getCurrentUserRole() const
-    {
-        juce::String username, password, accountInfo, role;
-        loadUserInfo(username, password, accountInfo, role);
-        return role;
-    }
+    void saveUserInfo(const juce::String& username,
+        const juce::String& password,
+        const juce::String& accountInfo,
+        const juce::String& role);
+
+    void loadUserInfo(juce::String& username,
+        juce::String& password,
+        juce::String& accountInfo,
+        juce::String& role) const;
+
+    void saveGuestInfo(juce::String& username,
+        juce::String& password);
+
+    void loadGuestInfo(juce::String& username,
+        juce::String& password) const;
+
+    void playRecordedAudio();
+    void loadAudioFileForPlayback(const juce::File& file);
+    void refreshSoundListFromStorage();
+    void saveSoundListToStorage();
+    void addSavedSound(const juce::String& name, const juce::String& path);
+
+    bool isOwnerLoggedIn() const;
+    bool isGuestLoggedIn() const;
+
+    void drawWaveform(juce::Graphics& g);
+    void drawClusterMap(juce::Graphics& g);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
